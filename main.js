804a25de90c4bde5f00de57845e02f3747646cdb
BThomas <bran_thom@yahoo.com>
2025-01-26 14:47:01 -0600

    setting up sonic

---
+++ main.js
@@ -0,0 +1,8 @@
+import k from "./kaplayCtx"
+
+k.loadSprite("chemical-bg","graphics/chemical-bg.png")
+k.loadSprite("platforms","graphics/platforms.png")
+k.loadSprite("sonic","graphics/sonic.png",{
+    sliceX: 8,
+    sliceY: 2,
+})
