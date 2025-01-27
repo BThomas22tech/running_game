187de12a314d4ef88ce3bc56dcb653186025b6ee
BThomas <bran_thom@yahoo.com>
2025-01-26 22:22:34 -0600

    moving background

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -14,5 +14,16 @@ export default function mainMenu() {
             k.opacity(0.8),
         ]),
     ];
-    const platforms = [k.add([k.sprite("platforms"), k.pos(0,450), k.scale(4)])]
+    const platformWidth = 1280
+    const platforms = [k.add([k.sprite("platforms"), k.pos(0,450), k.scale(4)]),
+                      k.add([k.sprite("platforms"), k.pos(platformWidth * 4,450), k.scale(4)])]
+
+k.onUpdate( () =>{
+    if (bgPieces[1].pos.x<0){
+        bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0)
+        bgPieces.push(bgPieces.shift())
+    }
+    bgPieces[0].move(-100,0)
+    bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0)
+})
 }
